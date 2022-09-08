98fb8f17bc9fd3dc2204d3a86c87fb3dd6b444f2
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-09-08 09:56:02 +0900

    list

---
+++ 4_3_list.py
@@ -0,0 +1,24 @@
+# list = [element,element]
+import random
+import statistics
+
+students = ["egoing", "sori","maru"]
+
+grades = [2,1,4]
+
+print("students[1]",students[1])
+print("len(students)",len(students))
+print(min(grades))
+
+print(max(grades))
+
+
+print(min(students))
+
+print(max(students))
+
+print("statistics.mean(grades)",statistics.mean(grades))
+
+print("random.choice(grades)",random.choice(grades))
+
+
