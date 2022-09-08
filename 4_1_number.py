918f93fe1aaae619670e7968c0342cbc3469af72
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-09-08 09:39:42 +0900

    number

---
+++ 4_1_number.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+print(-1)
+print(0)
+print(1) #int
+print(1.1) # float
+print('1+1', 1+1)
+
+print('1-1', 1-1)
+
+print('1*1', 1*1)
+
+print('1/1', 1/1)
+
+print('1%1', 1%1)
+
+print('math.pow(4,2)',math.sqrt(4))#제곱근
+print('math.pow(4,2)',math.pow(4,2))#제곱
+
+print('random.random()',random.random())
