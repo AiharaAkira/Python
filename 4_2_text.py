4a89216bbe5202ec15de5b5e51b9f2ac75920acb
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-09-08 09:47:06 +0900

    text

---
+++ 4_2_text.py
@@ -0,0 +1,21 @@
+print('hello world')
+print("hello world")
+
+print('''
+hello
+world
+ ''')
+
+
+print('''
+hello
+\n
+world
+ ''')
+
+
+print("'1'+'1'",'1'+'1')
+print('hello'*10)
+print(len('hello'*1000))
+
+print("'hello world'.replace('world','universe')",'hello world'.replace('world','universe'))
